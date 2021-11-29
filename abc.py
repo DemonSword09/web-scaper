6eb182324656cb34b4ce98ec7e2391d204302de5
DemonSword09 <72071947+DemonSword09@users.noreply.github.com>
2021-11-29 15:11:14 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+import webbrowser
+import time
+import re
+
+url = 'https://www.mrtechsonu.com/free-google-play-redeem-code-gift-card'
+res = requests.get(url)
+html_page = res.content
+soup = BeautifulSoup(html_page, 'html.parser')
+
+text = soup.find_all(text=True)
+text += soup.find_all('td')
+text += soup.find_all('tr')
+text += soup.find_all('li')
+
+file2 = open("MyFile.txt", "r")
+text2 = file2.read()
+file1 = open("MyFile.txt", "a")
+
+output = []
+for t in text:
+    p = t.string
+    if p == None:
+        continue
+    op = ''
+    for i in p:
+        if i not in (' ', '/', '{', ':', '}', ')', ']', '=', None):
+            op += i
+        else:
+            op = ''
+    if len(op) > 15:
+        if p not in text2:
+            output.append(op)
+            file1.write(p + '\n')
+print(*[str(j) + '.  ' + i + '\n' for j, i in enumerate(output)])
+for op in output[::-1]:
+    url = 'https://play.google.com/store?code=' + op
+    webbrowser.open(url, 2, False)
+    time.sleep(13)
+
+file1.close()
+file2.close()
+print("done!!!")
